bd6b649aef2bbc6cb1f07bb3407245ff4432f00d
andihat <106816033+andihat@users.noreply.github.com>
2023-03-07 19:37:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,31 +31,116 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-// Get a reference to the button element
-const scrollBtn = document.getElementById("scrollBtn");
 
-// Set the y-coordinate of the scroll trigger
+const scrollBtn = document.getElementById("scrollBtn");
 const scrollTrigger = 1000;
 scrollBtn.style.display = "none";
-// Add a scroll event listener to the window object
+bar.style.display = "none"
+
 window.onscroll = function() {
-  // Get the current y-coordinate of the scroll position
+  
   const y = window.scrollY;
 
-  // If the y-coordinate is greater than the scroll trigger, show the button
+  console.log(y)
+  
   if (y > scrollTrigger) {
     scrollBtn.style.display = "block";
   } else {
     scrollBtn.style.display = "none";
   }
+
+  if (y > scrollTrigger) {
+    bar.style.display = "block";
+    bar.classList.add('show2');
+  } else {
+    bar.style.display = "none";
+  }
+
+  if (y > 1000 && y < 1700) {
+    intro.style.display = "block";
+    intro.classList.add('show3');
+  } else if (y > 1800) {
+    intro.classList.remove('show3')
+    intro.classList.add('pH')
+  }else {
+    intro.classList.remove('pH')
+  }
+
+  if (y > 1800 && y < 4000) {
+    input.style.display = "block";
+    input.classList.add('show3');
+  } else if (y > 4000) {
+    input.classList.remove('show3')
+    input.classList.add('pH')
+  }else if (y < 1800) {
+    input.classList.remove('show3')
+    input.classList.remove('pH')
+  }
+
+ if (y > 4000 && y < 7816) {
+    ilMouse.style.display = "block";
+    ilMouse.classList.add('show3');
+  } else if (y > 7816) {
+    ilMouse.classList.remove('show3')
+    ilMouse.classList.add('pH')
+  }else if (y < 7816) {
+    ilMouse.classList.remove('show3')
+    ilMouse.classList.remove('show3')
+  } 
+
+  if (y > 7816 && y < 11356) {
+    laTastiera.style.display = "block";
+    laTastiera.classList.add('show3');
+  } else if (y > 11356) {
+    laTastiera.classList.remove('show3')
+    laTastiera.classList.add('pH')
+  }else if (y < 11356) {
+    laTastiera.classList.remove('show3')
+    laTastiera.classList.remove('show3')
+  }
+
+  if (y > 11356 && y < 12464) {
+    output.style.display = "block";
+    output.classList.add('show3');
+  } else if (y > 12464) {
+    output.classList.remove('show3')
+    output.classList.add('pH')
+  }else if (y < 12464) {
+    output.classList.remove('show3')
+    output.classList.remove('show3')
+  }
+
+  if (y > 12464 && y < 17392) {
+    monitor.style.display = "block";
+    monitor.classList.add('show3');
+  } else if (y > 17392) {
+    monitor.classList.remove('show3')
+    monitor.classList.add('pH')
+  }else if (y < 17392) {
+    monitor.classList.remove('show3')
+    monitor.classList.remove('show3')
+  }
+
+  if (y > 17392 && y < 19890) {
+    gliAltoparlanti.style.display = "block";
+    gliAltoparlanti.classList.add('show3');
+  } else if (y > 19890) {
+    gliAltoparlanti.classList.remove('show3')
+    gliAltoparlanti.classList.add('pH')
+  }else if (y < 19890) {
+    gliAltoparlanti.classList.remove('show3')
+    gliAltoparlanti.classList.remove('show3')
+  }
+
+
 };
 
-// Add the scrollByAmount function to the button's onclick event
+
 function scrollByAmount() {
     const startY = window.pageYOffset;
     const endY = startY + 1200;
     const distance = endY - startY;
-    const duration = 1000; // in milliseconds
+    const duration = 1000; 
   
     let startTime = null;
   
